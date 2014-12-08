69e5832278edb643bcca1998818403658ead97ee
Haydn Newport <haydn@catalyst.net.nz>
2014-12-08 20:50:48 +1300

    Add mock invite search backend

--- rsvp/urls.py
+++ rsvp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     # ex: /rsvp/5/
     url(r'^(?P<invite_id>\d+)/$', views.respond, name='respond'),
+    # ex: /rsvp/search/?q=Tom
+    url(r'^search.json\?(?P<query>.*)$', views.search, name='search'),
 )
--- rsvp/views.py
+++ rsvp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import json
+
 def index(request):
   return HttpResponse("This is the response form")
 
 def respond(request, invite_id):
   return HttpResponse("This is the specfic response form for invite {0}".format(invite_id))
+
+def search(request, query):
+
+  return HttpResponse("""[{
+    "match": "name",
+    "name": "Errol & Rosie",
+    "informal" : "Mum & Dad",
+    "invite_id" : 8
+    }]""")
