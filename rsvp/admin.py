ec23a559121ac03637a48c3ea95f8ec4657fdd61
Haydn Newport <haydn@catalyst.net.nz>
2014-12-19 08:39:10 +1300

    Add some relevant details to the admin

--- rsvp/admin.py
+++ rsvp/admin.py
@@ -7,6 +7,13 @@ class InviteAdmin(admin.ModelAdmin):
   list_display = ('name','informal_name', 'rsvp', 'number_attending')
   #list_filter = ('
   search_fields = ['name', 'informal_name']
+  readonly_fields = ('rsvp', 'attending', 'number_attending')
+
+class AlertAdmin(admin.ModelAdmin):
+  list_display = ('timestamp', 'reason', 'invitee')
+
+  def invitee(self, obj):
+    return obj.invite.informal_name
 
 admin.site.register(models.Invite, InviteAdmin)
-admin.site.register(models.Alert)
+admin.site.register(models.Alert, AlertAdmin)
